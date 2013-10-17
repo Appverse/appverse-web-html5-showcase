e18aac3c243ac8a8cd6c3bc98e9230735049b4ba
jesusjavierdediego <jesus.dediego.erles@gmail.com>
2013-10-17 12:21:07 +0200

    Included modified templates for distribution compatibility. Changed favicon. Included some mocks componentes for testing purposes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -256,7 +256,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>',
-          src: ['*.html', 'views/*.html'],
+          src: ['*.html', 'views/**/*.*', 'template/**/*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
