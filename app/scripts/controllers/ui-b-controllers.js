50ed1f1c5591d7c702930a53983fb5b48c88c2c1
jesusjavierdediego <jesus.dediego.erles@gmail.com>
2013-10-29 19:13:11 +0100

    Added Modal Window Demo widget

--- app/scripts/controllers/ui-b-controllers.js
+++ app/scripts/controllers/ui-b-controllers.js
@@ -76,4 +76,47 @@ angular.module('appverseClientIncubatorApp')
       })
 
 
+      
+.controller('ModalDemoCtrl', ['$scope', '$modal', '$log', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function () {
+    var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+        $scope.items = items;
+        $scope.selected = {
+          item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+          $modalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+          $modalInstance.dismiss('cancel');
+        };
+    };  
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: ModalInstanceCtrl,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+}])
+
+
+
+
 
