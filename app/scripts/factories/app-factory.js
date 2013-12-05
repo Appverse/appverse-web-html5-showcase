68dfce1f46e74853c1f1be45e02757fd5ec79979
Antoine Charnoz <aecz@gft.com>
2013-12-05 20:31:35 +0100

    - Formatted to comply with JSHint - Added RESTFactory dependency - Used RESTFactory to retrieve topics list

--- app/scripts/factories/app-factory.js
+++ app/scripts/factories/app-factory.js
@@ -1,57 +1,64 @@
+'use strict';
+
 angular.module('appverseClientIncubatorApp')
 
-    /*
-     * Factory Name: 'topics'
-     * This factory contains is RESTful for retrieving topics 
-     * from data (in this case'topics.json').
-     */
-  .factory('topics', ['$http', function ($http, utils) {
-    var path = 'views/topics/topics.json';
-    var topics = $http.get(path).then(function (resp) {
-      return resp.data.topics;
-    });
+/*
+ * Factory Name: 'topics'
+ * This factory contains is RESTful for retrieving topics
+ * from data (in this case'topics.json').
+ */
+.factory('topics', ['$http', 'RESTFactory',
+    function ($http, RESTFactory, utils) {
 
+        var path = 'views/topics/topics.json';
 
+        var topics = RESTFactory.rest_getAll(path);
 
-    var factory = {};
-    factory.all = function () {
-      return topics;
-    };
-    factory.get = function (id) {
-      return topics.then(function(){
-        return utils.findById(topics, id);
-      })
-    };
-    return factory;
-  }])
-   
-   /*
-    * Factory Name: 'utils'
-    * Contains methods for data finding (demo).
-    * 
-    * findById: Util for finding an object by its 'id' property among an array
-    * newRandomKey: Util for returning a randomKey from a collection that also isn't the current key
-    */
-  .factory('utils', function () {
+        console.log("topics", topics);
+
+        var factory = {};
+
+        factory.all = function () {
+            return topics;
+        };
+        factory.get = function (id) {
+            console.log("factory.get", id);
+            return topics.then(function () {
+                return utils.findById(topics, id);
+            });
+        };
+        return factory;
+    }])
+
+/*
+ * Factory Name: 'utils'
+ * Contains methods for data finding (demo).
+ *
+ * findById: Util for finding an object by its 'id' property among an array
+ * newRandomKey: Util for returning a randomKey from a collection that also isn't the current key
+ */
+.factory('utils', function () {
 
     return {
 
-      // 
-      findById: function findById(a, id) {
-        for (var i = 0; i < a.length; i++) {
-          if (a[i].id == id) return a[i];
-        }
-        return null;
-      },
+        //
+        findById: function findById(a, id) {
+            for (var i = 0; i < a.length; i++) {
+                if (a[i].id == id) {
+                    return a[i];
+                }
+            }
+            return null;
+        },
 
-      newRandomKey: function newRandomKey(coll, key, currentKey){
-        var randKey;
-        do {
-          randKey = coll[Math.floor(coll.length * Math.random())][key];
-        } while (randKey == currentKey);
-        return randKey;
-      }
+        newRandomKey: function newRandomKey(coll, key, currentKey) {
+            var randKey;
+            do {
+                randKey = coll[Math.floor(coll.length * Math.random())][key];
+            } while (randKey == currentKey);
+            return randKey;
+        }
 
     };
 
-  });
+});
