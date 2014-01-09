6ca17c6471b952b6be061b797e69d40d18ee4ea5
Antoine Charnoz <aecz@gft.com>
2014-01-09 19:40:41 +0100

    TMENU-50 - Beautified code - Added log in run function

--- app/scripts/api/modules/api-serverpush.js
+++ app/scripts/api/modules/api-serverpush.js
@@ -29,10 +29,8 @@
 // That is the reason it is not a dependency handled by bower.
 ////////////////////////////////////////////////////////////////////////////
 
-angular.module('AppServerPush', [
-        'AppSocketIO',
-        'AppConfiguration'])
-    /*
+angular.module('AppServerPush', ['AppSocketIO', 'AppConfiguration'])
+/*
      To make socket error events available across an app, in one of the controllers:
 
      controller('MyCtrl', function ($scope) {
@@ -40,27 +38,29 @@ angular.module('AppServerPush', [
             ...
      });
      */
-    .run(function (socket) {
+.run(['$log', 'socket',
+    function ($log, socket) {
+        $log.info('AppServerPush run');
         socket.forward('error');
-    })
-    /*
-     * Factory Name: 'SocketFactory'
-     * Although Socket.IO exposes an io variable on the window, it's better to encapsulate it
-     * into the AngularJS's Dependency Injection system.
-     * So, we'll start by writing a factory to wrap the socket object returned by Socket.IO.
-     * This will make easier to test the application's controllers.
-     *
-     * Notice that the factory wrap each socket callback in $scope.$apply.
-     * This tells AngularJS that it needs to check the state of the application and update
-     * the templates if there was a change after running the callback passed to it by using dirty checking.
-     * Internally, $http works in the same way. After some XHR returns, it calls $scope.$apply,
-     * so that AngularJS can update its views accordingly.
-     */
-    .factory('SocketFactory', ['$rootScope', 'socket',
+    }])
+/*
+ * Factory Name: 'SocketFactory'
+ * Although Socket.IO exposes an io variable on the window, it's better to encapsulate it
+ * into the AngularJS's Dependency Injection system.
+ * So, we'll start by writing a factory to wrap the socket object returned by Socket.IO.
+ * This will make easier to test the application's controllers.
+ *
+ * Notice that the factory wrap each socket callback in $scope.$apply.
+ * This tells AngularJS that it needs to check the state of the application and update
+ * the templates if there was a change after running the callback passed to it by using dirty checking.
+ * Internally, $http works in the same way. After some XHR returns, it calls $scope.$apply,
+ * so that AngularJS can update its views accordingly.
+ */
+.factory('SocketFactory', ['$rootScope', 'socket',
         function ($rootScope, socket) {
-            var factory = {};
+        var factory = {};
 
-            /*
+        /*
              @function
              @param eventName the name of the event/channel to be listened
              The communication is bound to rootScope.
@@ -69,16 +69,16 @@ angular.module('AppServerPush', [
              Use this method for background communication although the current scope is destyroyed.
              You should cancel communication manually or when the $rootScope object is destroyed.
              */
-            factory.listen = function (eventName, callback) {
-                socket.on(eventName, function () {
-                    var args = arguments;
-                    $rootScope.$apply(function () {
-                        callback.apply(socket, args);
-                    });
+        factory.listen = function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
                 });
-            };
+            });
+        };
 
-            /*
+        /*
              @function
              @param eventName the name of the event/channel to be sent to server
              @param scope the scope object to be bound to the listening.
@@ -87,40 +87,40 @@ angular.module('AppServerPush', [
              @description Establishes a communication listening an event/channel from server.
              It is bound to a given $scope object.
              */
-            factory.sendMessage = function (eventName, data, callback) {
-                socket.emit(eventName, data, function () {
-                    var args = arguments;
-                    $rootScope.$apply(function () {
-                        if (callback) {
-                            callback.apply(socket, args);
-                        }
-                    });
-                })
-            };
+        factory.sendMessage = function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            });
+        };
 
-            /*
+        /*
              @function
              The communication will be cancelled without regarding other consideration.
              No callback is passed.
              @description Cancels all communications to server.
              */
-            factory.unsubscribeCommunication = function (callback) {
-                socket.off(callback());
-            };
+        factory.unsubscribeCommunication = function (callback) {
+            socket.off(callback());
+        };
 
 
-            return factory;
+        return factory;
 
-        }]);
-
-
+    }]);
 
 //////////////////////////////////////////////////////////////////////////////
 // COMMON API - 0.1
 // PRIVATE MODULE (AppSocketIO)
 ////////////////////////////////////////////////////////////////////////////
 angular.module('AppSocketIO', ['AppConfiguration']).
-    provider('socket', ['SERVERPUSH_CONFIG', function (SERVERPUSH_CONFIG) {
+
+provider('socket', ['SERVERPUSH_CONFIG',
+    function (SERVERPUSH_CONFIG) {
 
         // when forwarding events, prefix the event name
         var prefix = 'socket:',
@@ -182,10 +182,10 @@ angular.module('AppSocketIO', ['AppConfiguration']).
                     }
                 },
 
-//                removeListener: function () {
-//                    var args = arguments;
-//                    return socket.removeListener.apply(socket, args);
-//                },
+                //                removeListener: function () {
+                //                    var args = arguments;
+                //                    return socket.removeListener.apply(socket, args);
+                //                },
 
                 forward: function (events, scope) {
                     if (events instanceof Array === false) {
