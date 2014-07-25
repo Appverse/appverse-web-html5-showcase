5111ab50b7460c4a74b3dcae408180b9dfe883bb
Antoine Charnoz <aecz@gft.com>
2014-07-25 18:06:10 +0200

    - Updated to bootstrap 3 and ui-bootstrap 0.11 - Added new bootstrap.scss - Removed conflicting body padding from cpustyle - Removed already included fade.css - Fixed navbar syntax - Added bootstrap fonts to server

--- Gruntfile.js
+++ Gruntfile.js
@@ -440,6 +440,12 @@ module.exports = function (grunt) {
                     '*ar-eg.js'
                 ]
             },
+            fonts: {
+                expand: true,
+                cwd: '<%= yeoman.app %>/bower_components/bootstrap-sass/fonts',
+                dest: '.tmp/fonts',
+                src: '*'
+            },
             png: {
                 expand: true,
                 cwd: '<%= yeoman.app %>',
@@ -451,7 +457,8 @@ module.exports = function (grunt) {
             server: [
                 'coffee',
                 'compass:server',
-                'copy:i18n'
+                'copy:i18n',
+                'copy:fonts'
             ],
             dist: [
                 'coffee',
