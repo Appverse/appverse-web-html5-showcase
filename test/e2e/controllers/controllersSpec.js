4dc0508fa707121e0c10c20264a8ec802dd3775f
Antoine Charnoz <aecz@gft.com>
2014-09-23 12:20:10 +0200

    - Replaced root variable with browser.baseUrl - Removed browser.get to speed up test suite

--- test/e2e/controllers/controllersSpec.js
+++ test/e2e/controllers/controllersSpec.js
@@ -2,12 +2,9 @@
 
 describe("E2E: Testing Controllers", function () {
 
-    var root = 'http://localhost:9090/';
-    browser.get(root);
-
     it('should have a working home page controller', function () {
         browser.setLocation('/home');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/home");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/home");
         //        element.all(by.css('[ui-view]')).then(function (items) {
         //            expect(items[0].getInnerHtml()).toContain('ng-controller="OauthLoginCtrl"');
         // });
--- test/e2e/routesSpec.js
+++ test/e2e/routesSpec.js
@@ -2,59 +2,56 @@
 
 describe("E2E: Testing Routes", function () {
 
-    var root = 'http://localhost:9090/';
-    browser.get(root);
-
     it('should have a title', function () {
         expect(browser.getTitle()).toEqual('Appverse Web HTML5 Incubator');
     });
 
     it('should jump to the /home path when / is accessed', function () {
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/home");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/home");
     });
 
     it('should have a working /topics route', function () {
         browser.setLocation('/topics');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/topics");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/topics");
     });
 
     it('should have a working /ui route', function () {
         browser.setLocation('/ui');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/ui");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/ui");
     });
 
     it('should have a working /cache route', function () {
         browser.setLocation('/cache/state1');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/cache/state1");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/cache/state1");
     });
 
     it('should have a working /detection route', function () {
         browser.setLocation('/detection');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/detection");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/detection");
     });
 
     it('should have a working /mobile route', function () {
         browser.setLocation('/mobile');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/mobile");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/mobile");
     });
 
     it('should have a working /websockets route', function () {
         browser.setLocation('/websockets');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/websockets");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/websockets");
     });
 
     it('should have a working /translation route', function () {
         browser.setLocation('/translation');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/translation");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/translation");
     });
 
     it('should have a working /performance/webworkers route', function () {
         browser.setLocation('/performance/webworkers');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/performance/webworkers");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/performance/webworkers");
     });
 
     it('should have a working /about route', function () {
         browser.setLocation('/about');
-        expect(browser.getLocationAbsUrl()).toBe(root + "#/about");
+        expect(browser.getLocationAbsUrl()).toBe(browser.baseUrl + "/#/about");
     });
 });
