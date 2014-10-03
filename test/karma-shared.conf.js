b5527137c4e8453a6d9b1ad397cfd06fea9cab68
Rodriguez Estivill, Marc <mrodriguezestivill@gmail.com>
2014-10-03 09:37:16 +0200

    fixed dependencies

--- test/karma-shared.conf.js
+++ test/karma-shared.conf.js
@@ -39,7 +39,7 @@ module.exports = function () {
             'app/bower_components/angular-ui-router/release/angular-ui-router.min.js',
             'app/bower_components/angular-cache/dist/angular-cache.min.js',
             'app/bower_components/angular-resource/angular-resource.min.js',
-            'app/bower_components/ng-grid/ng-grid-2.0.11.debug.js',
+            'app/bower_components/ng-grid/build/ng-grid.debug.js',
             'app/bower_components/socket.io-client/dist/socket.io.min.js',
 //            'http://cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min.js',
 
@@ -47,31 +47,31 @@ module.exports = function () {
             'app/bower_components/lodash/dist/lodash.underscore.min.js',
             'app/bower_components/restangular/dist/restangular.min.js',
 
-            'app/bower_components/appverse-web-html5-core/src/modules/api-cache.js',
-            'app/bower_components/appverse-web-html5-core/src/modules/api-configuration.js',
-            'app/bower_components/appverse-web-html5-core/src/modules/api-detection.js',
+            'app/scripts/api/modules/api-cache.js',
+            'app/scripts/api/modules/api-configuration.js',
+            'app/scripts/api/modules/api-detection.js',
  //            'app/scripts/api/modules/api-logging.js',
-            'app/bower_components/appverse-web-html5-core/src/modules/api-main.js',
+            'app/scripts/api/modules/api-main.js',
 
             'app/bower_components/lodash/dist/lodash.underscore.min.js',
             'app/bower_components/restangular/dist/restangular.min.js',
-            'app/bower_components/appverse-web-html5-core/src/modules/api-rest.js',
+            'app/scripts/api/modules/api-rest.js',
 
-            'app/bower_components/appverse-web-html5-security/src/modules/api-security.js',
+            'app/scripts/api/modules/api-security.js',
 
-            'app/bower_components/appverse-web-html5-core/src/modules/api-serverpush.js',
+            'app/scripts/api/modules/api-serverpush.js',
 
-            'app/bower_components/appverse-web-html5-core/src/modules/api-translate.js',
+            'app/scripts/api/modules/api-translate.js',
             'app/bower_components/angular-translate/angular-translate.min.js',
             'app/bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
-            'app/bower_components/angular-dynamic-locale/src/tmhDinamicLocale.js',
+            'app/bower_components/angular-dynamic-locale/src/tmhDynamicLocale.js',
 
-            'app/bower_components/appverse-web-html5-core/src/modules/api-performance.js',
+            'app/scripts/api/modules/api-performance.js',
 
-            'app/bower_components/appverse-web-html5-core/src/modules/api-utils.js',
+            'app/scripts/api/modules/api-utils.js',
 
-            'app/bower_components/appverse-web-html5-core/src/directives/*.js',
-            'app/bower_components/appverse-web-html5-security/src/directives/*.js',
+            'app/scripts/api/directives/*.js',
+            'app/scripts/api/directives/*.js',
             'app/scripts/app.js',
             'app/scripts/controllers/*.js',
             'app/scripts/factories/*.js',
