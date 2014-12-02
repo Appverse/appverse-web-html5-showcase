58037de23fa95f8e4b12d75fecd8fad208e0de5f
Antoine Charnoz <aecz@gft.com>
2014-12-02 19:34:55 +0100

    - Added- Added XML test reports for E2E

--- test/protractor-e2e.conf.js
+++ test/protractor-e2e.conf.js
@@ -1,10 +1,25 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['test/e2e/init.js', 'test/e2e/**/*.js'],
+    allScriptsTimeout: 60000,
     baseUrl: 'http://localhost:9090',
     capabilities: {
         browserName: 'phantomjs',
         'phantomjs.binary.path': 'node_modules/.bin/phantomjs' + (process.platform === 'win32' ? '.cmd' : ''),
         'phantomjs.cli.args': ['--ignore-ssl-errors=true', '--web-security=false']
+    },
+    onPrepare: function () {
+        require('jasmine-reporters');
+        var capsPromise = browser.getCapabilities();
+        capsPromise.then(function (caps) {
+            var browserName = caps.caps_.browserName.toUpperCase();
+            var browserVersion = caps.caps_.version;
+            var prePendStr = browserName + "-" + browserVersion + "-";
+            jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter("test/reports", true, true, prePendStr));
+        });
+    },
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
     }
 };
