7b0b057a67ce60f6b129490c7b0c1ee805d282dd
Antoine Charnoz <antoine.charnoz@gft.com>
2015-08-12 18:44:29 +0200

    - Added missing e2e tests

---
+++ test/e2e/detectionSpec.js
@@ -0,0 +1,18 @@
+/*jshint node:true */
+"use strict";
+
+describe("E2E: Testing detection view", function () {
+
+    beforeAll(function () {
+        browser.setLocation('detection');
+    });
+
+    it('should have a working /detection route', function () {
+        expect(browser.getLocationAbsUrl()).toBe("/detection");
+    });
+
+    it('should have a initial bandwidth at 0', function () {
+        expect(element(by.binding('detect.bandwidth')).getText()).toBe("0");
+    });
+
+});
---
+++ test/e2e/restSpec.js
@@ -0,0 +1,49 @@
+/*jshint node:true */
+'use strict';
+
+describe('E2E: Testing rest view', function () {
+
+    beforeAll(function () {
+        browser.setLocation('demos/rest');
+    });
+
+    it('should have a working /demos/rest route', function () {
+
+        expect(browser.getLocationAbsUrl()).toBe('/demos/rest');
+    });
+
+    it('should open user modal and cancel it', function () {
+
+        element(by.buttonText('Add User')).click();
+
+        browser.waitForAngular();
+
+        element.all(by.css('.modal-title')).then(function (items) {
+            expect(items.length).toBe(1);
+            expect(items[0].getText()).toBe('User');
+        });
+
+        element(by.buttonText('Cancel')).click();
+
+        browser.waitForAngular();
+    });
+
+    it('should open user modal and save it', function () {
+
+        element.all(by.css('button[ng-click="openUser(row.entity)"]')).then(function (items) {
+            items[0].click();
+        });
+
+        browser.waitForAngular();
+
+        element.all(by.css('.modal-title')).then(function (items) {
+            expect(items.length).toBe(1);
+            expect(items[0].getText()).toBe('User');
+        });
+
+        element(by.buttonText('Save')).click();
+
+        browser.waitForAngular();
+    });
+
+});
