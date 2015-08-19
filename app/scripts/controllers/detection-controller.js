c077d9ccd373952f9e3e6d5c722e2a336aba1abb
Louis, Kamau <Kamau.Louis@gft.com>
2015-08-19 12:49:34 +0200

    Removed webworker code from performance controller in order to create it's own controller.  Added the new webworker controller to index.html

--- app/scripts/controllers/detection-controller.js
+++ app/scripts/controllers/detection-controller.js
@@ -22,7 +22,9 @@ angular.module('App.Controllers')
 
 .controller('DetectionController',
     function ($log, $scope, Detection) {
+
         'use strict';
+
         $log.debug('DetectionController loading');
 
         $scope.detect = Detection;
