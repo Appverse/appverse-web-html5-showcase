768c670e1342ef52c476cd3ac45ccff3d0fa64f2
Antoine Charnoz <antoine.charnoz@gft.com>
2015-09-10 12:44:41 +0200

    - Added chai-as-promised - Added cucumber option to shell task - Added test:cucumber task - Removed unneeded waitPlugin - Fixed reports folder creation - Added coverage report for each browser except IE - Added after hook to save a screenshot - Simplified all steps using chai should eventually syntax - Fixed progress bar test condition

--- test/e2e/features/performance.feature
+++ test/e2e/features/performance.feature
@@ -1,15 +1,12 @@
-  Feature: Performance
+Feature: Performance
+    Narrative:
+    In order to compare testing frameworks for HTML5 apps
+    As a user
+    I want to be able to execute end to end tests for the AppVerse Showcase
 
-  Narrative:
-  In order to compare testing frameworks for HTML5 apps
-  As a user
-  I want to be able to execute end to end tests for the AppVerse Showcase
-
-Scenario: Performance WebWorkers
-  Meta:
-@scPerfWebWorkers
-
-  Given I go to Performance/WebWorkers
-  When shows blue result
-    And I click on Run button
-  Then shows result in green after execution
+    Scenario: Performance WebWorkers
+        Meta: @scPerfWebWorkers
+        Given I go to Performance/WebWorkers
+        When shows blue result
+        And I click on Run button
+        Then shows result in green after execution
--- test/protractor-cucumber.conf.js
+++ test/protractor-cucumber.conf.js
@@ -1,9 +1,8 @@
 /*jshint node:true */
 "use strict";
 
-var waitPlugin = require('./waitPlugin');
 var istanbul = require('istanbul');
-var collector = new istanbul.Collector();
+var fs = require('fs');
 
 exports.config = {
     seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
@@ -15,44 +14,48 @@ exports.config = {
     allScriptsTimeout: 60000,
     getPageTimeout: 20000,
     baseUrl: 'http://localhost:9003',
-    //    baseUrl: 'https://appverse.gftlabs.com/showcase-html5/new',
     framework: 'cucumber',
-    resultJsonOutputFile: 'test/cucumber-results.json',
+    resultJsonOutputFile: 'test/reports/e2e/cucumber-results.json',
     multiCapabilities: [
         // {
         //     browserName: 'phantomjs',
         //     'phantomjs.binary.path': require('phantomjs').path,
         //     'phantomjs.cli.args': ['--ignore-ssl-errors=true', '--web-security=false'],
-        // }
-        //,{
-        //     browserName: 'chrome'
-        // }
-        //,
+        // },
         {
-            browserName: 'firefox'
-        }
-        //,{ Not working on Mac OSX (at least) due to problem installing SafariDriver
+            browserName: 'chrome'
+        },
+        // {
+        //     browserName: 'firefox'
+        // },
+        // { Not working on Mac OSX (at least) due to problem installing SafariDriver
         //     browserName: 'safari'
-        // }
-        // , {
+        // },
+        // {
         //     browserName: 'internet explorer'
         // }
     ],
-    plugins: [{
-        path: './waitPlugin.js'
-    }],
-    onPrepare: function () {
-        browser.collector = collector;
+    onPrepare: function() {
+        browser.collector = new istanbul.Collector();
+        if (!fs.existsSync('test/reports')) {
+            fs.mkdirSync('test/reports');
+        }
+        if (!fs.existsSync('test/reports/e2e')) {
+            fs.mkdirSync('test/reports/e2e');
+        }
     },
-    onComplete: function () {
+    onComplete: function() {
+
+        var browserName = browser.getProcessedConfig().value_.capabilities.browserName;
+        if (browserName === 'internet explorer') {
+            return;
+        }
 
         istanbul.Report
             .create('lcov', {
-                dir: 'test/coverage/e2e'
+                dir: 'test/coverage/e2e/' + browserName
             })
             .writeReport(browser.collector, true);
-
-        waitPlugin.resolve();
     },
     jasmineNodeOpts: {
         showColors: true,
